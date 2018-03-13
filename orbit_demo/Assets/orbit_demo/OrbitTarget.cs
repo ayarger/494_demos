4cb86932dd3f1db5a3bd82c45127c07d3abdfd7c
Austin Yarger <ayarger@umich.edu>
2018-03-13 17:04:15 -0400

    Initial Commit. Added Orbit Demo.

---
+++ orbit_demo/Assets/orbit_demo/OrbitTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitTarget : MonoBehaviour {
+
+	public Transform target;
+	public float rate = 4.0f;
+	public float radius = 4.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + new Vector3 (Mathf.Cos (Time.time * rate), Mathf.Sin (Time.time * rate), 0) * radius;
+	}
+}
